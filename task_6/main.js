4a74f594135a73465a024f34fafb5d9b49622ba9
Masha <77979222+sobolevamasha@users.noreply.github.com>
2022-07-07 22:13:40 +0300

    add toggle

--- task_6/main.js
+++ task_6/main.js
@@ -1,1 +1,24 @@
-let procent = 20;
+//если  у тогла есть класс discount, то иннер хтмл = дата-таргет/procent
+//элсе иннер хтмл = дата таргет
+
+const btn = document.querySelector('.main__toggle');
+btn.onclick = function () {
+    btn.classList.toggle('discount');
+}
+
+const priceDivs = document.querySelectorAll('.tariff__price');
+let procent = 5;
+
+priceDivs.forEach(priceDiv => {
+    const setPrice = () => {
+        let price = priceDiv.getAttribute('data-target');
+        let discount = btn.classList.contains('discount');
+
+        if (discount) {
+            priceDiv.innerHTML = Math.round(price / procent);
+        } else {
+            priceDiv.innerHTML = price;
+        }
+    }
+    setPrice();
+});
