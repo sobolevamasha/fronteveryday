397c89a1e0e40bf6f5ed6ae965d47ceb2ca1dbab
Masha <77979222+sobolevamasha@users.noreply.github.com>
2022-07-07 23:23:53 +0300

    fix

--- task_6/main.js
+++ task_6/main.js
@@ -4,21 +4,21 @@
 const btn = document.querySelector('.main__toggle');
 btn.onclick = function () {
     btn.classList.toggle('discount');
-}
 
-const priceDivs = document.querySelectorAll('.tariff__price');
-let procent = 5;
+    const priceDivs = document.querySelectorAll('.tariff__price');
+    let procent = 0.35;
 
-priceDivs.forEach(priceDiv => {
-    const setPrice = () => {
-        let price = priceDiv.getAttribute('data-target');
-        let discount = btn.classList.contains('discount');
+    priceDivs.forEach(priceDiv => {
+        const setPrice = () => {
+            let price = priceDiv.getAttribute('data-target');
+            let discount = btn.classList.contains('discount');
 
-        if (discount) {
-            priceDiv.innerHTML = Math.round(price / procent);
-        } else {
-            priceDiv.innerHTML = price;
+            if (discount) {
+                priceDiv.innerHTML = ((price * 12) - ((price * 12) * procent) ).toFixed(2);
+            } else {
+                priceDiv.innerHTML = price;
+            }
         }
-    }
-    setPrice();
-});
+        setPrice();
+    });
+}
