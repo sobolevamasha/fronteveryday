b643274e9e64da8f387541171a29d0cd4d101552
Masha <77979222+sobolevamasha@users.noreply.github.com>
2022-07-14 22:34:07 +0300

    add mobile

--- task_7/app/js/main.js
+++ task_7/app/js/main.js
@@ -28,7 +28,7 @@ window.onload = function () {
         let date = new Date();
         let h = date.getHours();
         let m = date.getMinutes();
-        
+
 
         function MakeTwoDigits(m) {
             if (m >= 0 && m <= 9) {
@@ -38,37 +38,90 @@ window.onload = function () {
             }
         }
 
-        body.classList.add('day');
+        body.style.backgroundImage = 'url(../images/day.jpg)'
+        //body.classList.add('day');
+
+
 
         dateDiv.innerHTML = h + ':' + MakeTwoDigits(m);
-        if (date.getHours() > 4 && date.getHours() < 12) {
-            greetIcon.src = 'images/sun.svg'
-            greetText.innerText = 'Good morning, it`s currently'
+        console.log(h, m);
+
+        function setContent() {
+            if (h >= 4 && h <= 11) {
+                greetIcon.src = 'images/sun.svg'
+                greetText.innerText = 'Good morning, it`s currently'
+                if($(window).width() < 600) {
+                    greetText.innerText = 'Good morning'
+                }
+
+
+            }
+            else if (h >= 12 && h <= 15) {
+                greetIcon.src = 'images/moon.svg'
+                greetText.innerText = 'Good day, it`s currently'
+                if($(window).width() < 600) {
+                    greetText.innerText = 'Good day'
+                }
+
+            }
+            else if (h >= 16 && h <= 21) {
+                greetIcon.src = 'images/moon.svg'
+                greetText.innerText = 'Good evening, it`s currently'
+                if($(window).width() < 600) {
+                    greetText.innerText = 'Good evening'
+                }
+
+            } else {
+                greetIcon.src = 'images/moon.svg'
+                greetText.innerText = 'Good night, it`s currently'
+                if($(window).width() < 600) {
+                    greetText.innerText = 'Good night'
+                }
+                //body.classList.add('night');
+                body.style.backgroundImage = 'url(../images/night.jpg)'
+                addBlock.classList.add('night');
+            }
         }
-        if ((date.getHours() > 12 && date.getHours() < 16)) {
-            greetIcon.src = 'images/moon.svg'
-            greetText.innerText = 'Good day, it`s currently'
-        }
-        if ((date.getHours() > 16 && date.getHours() < 22)) {
-            greetIcon.src = 'images/moon.svg'
-            greetText.innerText = 'Good evening, it`s currently'
-            
-        }
-        else {
-            greetIcon.src = 'images/moon.svg'
-            greetText.innerText = 'Good night, it`s currently'
-            body.classList.add('night');
-            addBlock.classList.add('night');
-        }
+        setContent(h, m);
     }, 1000);
 
 };
 
+const timeZone = document.querySelector('.time-zone');
+timeZone.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+const weekDay = document.querySelector('.week-day');
+const yearDay = document.querySelector('.year-day');
+const weekNumber = document.querySelector('.week-number');
+
+(function() {
+    let days = ['7','1','2','3','4','5','6'];
+    Date.prototype.getDayName = function() {
+        return days[ this.getDay() ];
+    };
+    Date.prototype.getWeek = function() {
+        var onejan = new Date(this.getFullYear(), 0, 1);
+        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7 - 1);
+    }
+})();
+const now = new Date();
+weekDay.innerHTML = now.getDayName();
+weekNumber.innerHTML = (new Date()).getWeek();
+let start = new Date(now.getFullYear(), 0, 0);
+let diff = now - start;
+let oneDay = 1000 * 60 * 60 * 24;
+let day = Math.floor(diff / oneDay);
+yearDay.innerHTML = day;
+
+
 const switchBtn = document.querySelector('.main__btn');
+const btnText = document.querySelector('.btn-text');
 const addBlock = document.querySelector('.add');
 const topBlock = document.querySelector('.main__wrap');
 const container = document.querySelector('.container');
 const quoteBlock = document.querySelector('.main__quote-wrap');
+
+
 
 switchBtn.onclick = function () {
     switchBtn.classList.toggle('clicked');
@@ -77,4 +130,6 @@ switchBtn.onclick = function () {
     container.classList.toggle('half-sized');
     quoteBlock.style.display = quoteBlock.style.display === 'none' ? '' : 'none';
     topBlock.style.justifyContent = topBlock.style.justifyContent === 'center' ? '' : 'center';
+    btnText.innerHTML =
+    (btnText.innerHTML === 'less') ? btnText.innerHTML = 'more' : btnText.innerHTML = 'less';
 }
