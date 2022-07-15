77597a9a5aa1d61c1a7eedf1a685f37e32e9757d
Masha <77979222+sobolevamasha@users.noreply.github.com>
2022-07-16 01:38:56 +0300

    add mobile location YMap

--- task_7/app/js/main.js
+++ task_7/app/js/main.js
@@ -50,31 +50,29 @@ window.onload = function () {
             if (h >= 4 && h <= 11) {
                 greetIcon.src = 'images/sun.svg'
                 greetText.innerText = 'Good morning, it`s currently'
-                if($(window).width() < 600) {
+                if ($(window).width() < 600) {
                     greetText.innerText = 'Good morning'
                 }
 
 
-            }
-            else if (h >= 12 && h <= 15) {
+            } else if (h >= 12 && h <= 15) {
                 greetIcon.src = 'images/moon.svg'
                 greetText.innerText = 'Good day, it`s currently'
-                if($(window).width() < 600) {
+                if ($(window).width() < 600) {
                     greetText.innerText = 'Good day'
                 }
 
-            }
-            else if (h >= 16 && h <= 21) {
+            } else if (h >= 16 && h <= 21) {
                 greetIcon.src = 'images/moon.svg'
                 greetText.innerText = 'Good evening, it`s currently'
-                if($(window).width() < 600) {
+                if ($(window).width() < 600) {
                     greetText.innerText = 'Good evening'
                 }
 
             } else {
                 greetIcon.src = 'images/moon.svg'
                 greetText.innerText = 'Good night, it`s currently'
-                if($(window).width() < 600) {
+                if ($(window).width() < 600) {
                     greetText.innerText = 'Good night'
                 }
                 //body.classList.add('night');
@@ -94,12 +92,12 @@ const weekDay = document.querySelector('.week-day');
 const yearDay = document.querySelector('.year-day');
 const weekNumber = document.querySelector('.week-number');
 
-(function() {
-    let days = ['7','1','2','3','4','5','6'];
-    Date.prototype.getDayName = function() {
-        return days[ this.getDay() ];
+(function () {
+    let days = ['7', '1', '2', '3', '4', '5', '6'];
+    Date.prototype.getDayName = function () {
+        return days[this.getDay()];
     };
-    Date.prototype.getWeek = function() {
+    Date.prototype.getWeek = function () {
         var onejan = new Date(this.getFullYear(), 0, 1);
         return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7 - 1);
     }
@@ -131,5 +129,44 @@ switchBtn.onclick = function () {
     quoteBlock.style.display = quoteBlock.style.display === 'none' ? '' : 'none';
     topBlock.style.justifyContent = topBlock.style.justifyContent === 'center' ? '' : 'center';
     btnText.innerHTML =
-    (btnText.innerHTML === 'less') ? btnText.innerHTML = 'more' : btnText.innerHTML = 'less';
+        (btnText.innerHTML === 'less') ? btnText.innerHTML = 'more' : btnText.innerHTML = 'less';
 }
+
+const place = document.querySelector('.main__place');
+// Создание обработчика для события window.onLoad
+YMaps.jQuery(function () {
+    // Создание экземпляра карты и его привязка к созданному контейнеру
+    var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]),
+
+        // Центр карты
+        center,
+
+        // Масштаб
+        zoom = 10;
+
+    // Получение информации о местоположении пользователя
+    if (YMaps.location) {
+        center = new YMaps.GeoPoint(YMaps.location.longitude, YMaps.location.latitude);
+
+        if (YMaps.location.zoom) {
+            zoom = YMaps.location.zoom;
+        }
+
+        map.openBalloon(center, "Место вашего предположительного местоположения:<br/>" +
+            (YMaps.location.country || "") +
+            (YMaps.location.region ? ", " + YMaps.location.region : "") +
+            (YMaps.location.city ? ", " + YMaps.location.city : "")
+        )
+    } else {
+        center = new YMaps.GeoPoint(37.64, 55.76);
+    }
+
+    // Установка для карты ее центра и масштаба
+    map.setCenter(center, zoom);
+
+    console.log(YMaps.location.country);
+    console.log(YMaps.location.region);
+    console.log(YMaps.location.city);
+});
+
+place.innerHTML = (YMaps.location.city + ", " + YMaps.location.country);
